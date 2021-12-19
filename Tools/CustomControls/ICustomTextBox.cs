caeff17f89be84d04aa0db3d8a8f85177b41f588
On3s228 <77531256+On3s228@users.noreply.github.com>
2021-12-19 15:02:54 +0300

    some minor changes

---
+++ Tools/CustomControls/ICustomTextBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_Planner.CustomControls
+{
+    public interface ICustomTextBox
+    {
+        string Text { get; set; }
+        Color BorderColor { get; set; }
+    }
+}
